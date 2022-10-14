77401d21df7514a8d9a886e1eb4cc50565c470a0
aguileracastillo <102373301+aguileracastillo@users.noreply.github.com>
2022-10-13 21:44:55 -0500

    Playful

--- script/data_chapter3.R
+++ script/data_chapter3.R
@@ -2,16 +2,50 @@
 ## Secondary data
 
 #### Install packages
+library(tidyverse)
+library(eurostat)
 library(Rilostat)
 library(shiny)
 library(OECD)
 library(WDI)
+library(countrycode)
 
 
 
 #### ILOSTAT
 runExplorer()
-get_ilostat_toc()
+ilostat_toc <- get_ilostat_toc() ## table of contents --> id
+ilostat_dic <- get_ilostat_dic("indicator") ## dictionary
+## Employment by sex and institutional sector - Annual
+employment_sex_sector <- get_ilostat("EMP_TEMP_SEX_INS_NB_A")
+## Public employment by sectors and sub-sectors of national acct
+pub_emp_by_sector <- get_ilostat("PSE_TPSE_GOV_NB_A")
+## Trade Union Density rate (%)
+union_density <- get_ilostat("ILR_TUMT_NOC_RT_A")
+union_density$REGION <- NA
+union_density$EU28 <- NA
+
+## adds region column
+union_density$REGION <- countrycode(union_density$ref_area, 
+                                  origin = "iso3c",
+                                  destination = "region",
+                                  warn = TRUE,
+                                  nomatch = NA,
+                                  custom_dict = NULL,
+                                  custom_match = NULL,
+                                  origin_regex = NULL)
+
+## adds EU membership (before 2015 // EU28)
+union_density$EU28 <- countrycode(union_density$ref_area, 
+                                    origin = "iso3c",
+                                    destination = "eu28",
+                                    warn = TRUE,
+                                    nomatch = NA,
+                                    custom_dict = NULL,
+                                    custom_match = NULL,
+                                    origin_regex = NULL)
+
+union_density_EU28 %>% filter(ref_area == "EST")
 
 
 #### OECDstat - Government at a Glance (error)
